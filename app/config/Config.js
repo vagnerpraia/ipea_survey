fa8b06d3d433f9c22dfaf009fed7a1ae9c65e373
denise-silva <dssilva.2689@gmail.com>
2017-03-29 17:50:40 -0300

    form configurações

--- app/config/Config.js
+++ app/config/Config.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, Container, Content, Header, Icon, Text, Title } from 'native-base';
+import { Button, Container, Content, Header, Icon, ListItem, Text, Title, Radio, Right } from 'native-base';
+import TextField from 'react-native-md-textinput';
+import Selection from 'react-native-selection';
 
 import { exitApp } from '../Util';
 
 export default class Config extends Component {
+    returnDataOnSelection(e){
+        alert('Value : ' + e.value + ' Name : ' + e.name);
+    }
     render() {
+      const uf = [
+          {
+            name: 'RJ',
+            value: 1,
+          },
+          {
+            name: 'RN',
+            value: 2,
+          },
+          {
+            name: 'RO',
+            value: 3,
+          },
+        ];
+
+        const municipio = [
+          {
+            name: 'Rio de Janeiro',
+            value: 1,
+          },
+          {
+            name: 'São Gonçalo',
+            value: 2,
+          },
+          {
+            name: 'Duque de Caxias',
+            value: 3,
+          },
+        ];
         return (
             <Container style={styles.container}>
                 <Header>
@@ -20,11 +54,47 @@ export default class Config extends Component {
                     </Button>
                 </Header>
                 <Content>
-                    <Text style={styles.text}>Config 1</Text>
-                    <Text style={styles.text}>Config 2</Text>
-                    <Text style={styles.text}>Config 3</Text>
-                    <Text style={styles.text}>Config 4</Text>
-                    <Text style={styles.text}>Config 5</Text>
+                  <TextField
+                    label={'Nome do aplicador'}
+                    highlightColor={'#00BCD4'}
+                    dense={true}
+                  />
+                  <TextField
+                    label={'Nome do entrevistado'}
+                    highlightColor={'#00BCD4'}
+                    dense={true}
+                  />
+                  <Text style={{color: '#999999'}}>UF</Text>
+                <Selection
+                  title="Selecione UF"
+                  options={uf}
+                  onSelection={(e)=>this.returnDataOnSelection.bind(this, e)}
+                  style={{
+                    body: null,
+                    option: null,
+                  }}
+                />
+                <Text style={{color: '#999999'}}>Município</Text>
+                <Selection
+                  title="Selecione Município"
+                  options={municipio}
+                  onSelection={(e)=>this.returnDataOnSelection.bind(this, e)}
+                  style={{
+                    body: null,
+                    option: null,
+                  }}
+                />
+                <TextField
+                  label={'Localidade '}
+                  highlightColor={'#00BCD4'}
+                  dense={true}
+                />
+                <ListItem selected={false}>
+                    <Text>Urbana</Text>
+                    <Right>
+                      <Radio selected={false} />
+                    </Right>
+                </ListItem>
                 </Content>
             </Container>
         );

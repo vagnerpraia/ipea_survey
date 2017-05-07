28f32f760a4990cbbfbcb099104af747e10a0d91
vagnerpraia <vagnerpraia@gmail.com>
2017-05-07 20:43:32 -0300

    Ajustes na configução

--- app/component/config/Config.js
+++ app/component/config/Config.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Alert, StyleSheet, ToastAndroid, View } from 'react-native';
-import { Button, Container, Content, List, Header, Icon, ListItem, Picker, Text, Title } from 'native-base';
+import { Body, Button, Container, Content, Left, List, Header, Icon, ListItem, Picker, Text, Title } from 'native-base';
 import TextField from 'react-native-md-textinput';
 import RadioForm from 'react-native-simple-radio-button';
 import RNFetchBlob from 'react-native-fetch-blob';
 
 import { UF_MUNICIPIO, radio_localizacao, radio_loc_diferenciada } from './BdConfig';
-import { exitApp } from '../../Util';
+import { styles } from './../../Styles';
 
 const Item = Picker.Item;
 
@@ -20,92 +20,120 @@ export default class Config extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            nome_aplicador: '',
+            nome_entrevistado: '',
+            selected_uf: 'ac',
+            selected_municipio: 0,
+            localidade: '',
+            localizacao: '',
+            loc_diferenciada: '',
+            barragem: '',
+
+            mode: Picker.MODE_DIALOG,
+        }
+    }
+
+    componentWillMount(){
         let file_path = dir_file + 'config.json';
         fs.exists(file_path).then((exist) => {
             if(exist){
                 fs.readFile(file_path, 'utf8').then((data) => {
-                    console.log(data);
+                    let json = JSON.parse(data);
+
+                    this.state.nome_aplicador = json.nome_aplicador;
+                    this.state.nome_entrevistado = json.nome_entrevistado;
+                    this.state.selected_uf = json.selected_uf;
+                    this.state.selected_municipio = json.selected_municipio;
+                    this.state.localidade = json.localidade;
+                    this.state.localizacao = json.localizacao;
+                    this.state.loc_diferenciada = json.loc_diferenciada;
+                    this.state.barragem = json.barragem;
+
+                    if(this.state.selected_uf == undefined) this.state.selected_uf = 'ac';
+                    if(this.state.selected_municipio == undefined) this.state.selected_municipio = 0;
+
+                    this.forceUpdate();
                 });
             }
         });
     }
 
-    state = {
-        nome_aplicador: '',
-        nome_entrevistado: '',
-        selected_uf: 'ac',
-        selected_municipio: 0,
-        localidade: '',
-        localizacao: '',
-        loc_diferenciada: '',
-        barragem: '',
+    voltar(){
+        let content = {
+            "nome_aplicador": this.state.nome_aplicador,
+            "nome_entrevistado": this.state.nome_entrevistado,
+            "uf": this.state.selected_uf,
+            "municipio": this.state.selected_municipio,
+            "localidade": this.state.localidade,
+            "localizacao": this.state.localizacao,
+            "loc_diferenciada": this.state.loc_diferenciada,
+            "barragem": this.state.barragem
+        };
 
-        mode: Picker.MODE_DIALOG,
+        let data = JSON.stringify(content);
+
+        let file_path = dir_file + 'config.json';
+        fs.exists(file_path).then((exist) => {
+            if(exist){
+                fs.writeFile(file_path, data, 'utf8').then(() => {
+                    ToastAndroid.showWithGravity('Configração salva', ToastAndroid.SHORT, ToastAndroid.CENTER);
+                });
+            }else{
+                fs.createFile(file_path, data, 'utf8');
+                ToastAndroid.showWithGravity('Configração salva', ToastAndroid.SHORT, ToastAndroid.CENTER);
+            }
+        });
+
+        this.props.navigator.replacePreviousAndPop({
+            name: 'home'
+        });
     }
 
     render() {
         let { nome_aplicador, nome_entrevistado, localidade, localizacao, loc_diferenciada, barragem } = this.state;
-
         var make = UF_MUNICIPIO[this.state.selected_uf];
-
-        var config = {
-            "nome_aplicador": nome_aplicador,
-            "nome_entrevistado": nome_entrevistado,
-            "uf": make.name,
-            "municipio": make.municipios[this.state.selected_municipio],
-            "localidade": localidade,
-            "localizacao": localizacao,
-            "loc_diferenciada": loc_diferenciada,
-            "barragem": barragem
-        };
-
-        var createFileConfig = (content) => {
-            var file_path = dir_file + 'config.json';
-            var data = JSON.stringify(content);
-            fs.exists(file_path).then((exist) => {
-                if(exist){
-                    fs.writeFile(file_path, data, 'utf8').then(() => {
-                        ToastAndroid.showWithGravity('Configração salva', ToastAndroid.SHORT, ToastAndroid.CENTER);
-                    });
-                }else{
-                    fs.createFile(file_path, data, 'utf8');
-                    ToastAndroid.showWithGravity('Configração salva', ToastAndroid.SHORT, ToastAndroid.CENTER);
-                }
-            });
-        };
 
         return (
             <Container style={styles.container}>
-                <Header>
-                    <Button transparent onPress={() => { this.props.navigator.pop(); }}>
-                        <Icon name='ios-arrow-back' />
-                    </Button>
-
-                    <Title>Configuração</Title>
-
-                    <Button transparent onPress={exitApp}>
-                        <Icon name='ios-close' />
-                    </Button>
+                <Header style={styles.header}>
+                    <Left>
+                        <Button transparent onPress={() => {this.voltar()}}>
+                            <Icon name='ios-arrow-back' />
+                        </Button>
+                    </Left>
+                    <Body style={styles.bodyHeader}>
+                        <View>
+                            <Title>Questionário</Title>
+                        </View>
+                    </Body>
                 </Header>
+
                 <Content>
                     <View style={styles.view}>
                         <TextField
                             label={'Nome do aplicador'}
                             value={nome_aplicador}
                             labelColor={'#000000'}
-                            onChangeText={ (nome_aplicador) => this.setState({ nome_aplicador }) }
+                            onChangeText={(value) => {
+                                this.state.nome_aplicador = value
+                            }}
                             highlightColor={'#00BCD4'}
                             dense={true}
                         />
+
                         <TextField
                             label={'Nome do entrevistado'}
                             value={nome_entrevistado}
                             labelColor={'#000000'}
-                            onChangeText={ (nome_entrevistado) => this.setState({ nome_entrevistado }) }
+                            onChangeText={(value) => {
+                                this.state.nome_entrevistado = value
+                            }}
                             highlightColor={'#00BCD4'}
                             dense={true}
                         />
-                        <Text style={styles.texttitle} >UF</Text>
+
+                        <Text style={styles.texttitle}>UF</Text>
                         <Picker
                             selectedValue={this.state.selected_uf}
                             onValueChange={(selected_uf) => this.setState({selected_uf, selected_municipio: 0})}>
@@ -117,7 +145,8 @@ export default class Config extends Component {
                                 />
                             ))}
                         </Picker>
-                        <Text style={styles.texttitle} >Município</Text>
+
+                        <Text style={styles.texttitle}>Município</Text>
                         <Picker
                             selectedValue={this.state.selected_municipio}
                             key={this.state.selected_uf}
@@ -127,18 +156,20 @@ export default class Config extends Component {
                                     key={this.state.selected_uf + '_' + selected_municipio}
                                     value={selected_municipio}
                                     label={modelName}
-                                    />
+                                />
                             ))}
                         </Picker>
+
                         <TextField
                             label={'Localidade '}
                             value={localidade}
                             labelColor={'#000000'}
-                            onChangeText={ (localidade) => this.setState({ localidade }) }
+                            onChangeText={ (localidade) => this.setState({localidade}) }
                             highlightColor={'#00BCD4'}
                             dense={true}
                         />
-                        <Text style={styles.texttitle} >Localização/Zona</Text>
+
+                        <Text style={styles.texttitle}>Localização/Zona</Text>
                         <RadioForm
                             radio_props={radio_localizacao}
                             initial={null}
@@ -148,7 +179,8 @@ export default class Config extends Component {
                             style={styles.radioForm}
                             onPress={(localizacao) => {this.setState({localizacao})}}
                         />
-                        <Text style={styles.texttitle} >Localização diferenciada</Text>
+
+                        <Text style={styles.texttitle}>Localização diferenciada</Text>
                         <RadioForm
                             radio_props={radio_loc_diferenciada}
                             initial={null}
@@ -158,19 +190,15 @@ export default class Config extends Component {
                             style={styles.radioForm}
                             onPress={(loc_diferenciada) => {this.setState({loc_diferenciada})}}
                         />
+
                         <TextField
                             label={'Barragem'}
                             value={barragem}
                             labelColor={'#000000'}
-                            onChangeText={ (barragem) => this.setState({ barragem }) }
+                            onChangeText={ (barragem) => this.setState({barragem}) }
                             highlightColor={'#00BCD4'}
                             dense={true}
                         />
-                        <Button style={{ marginTop: 10 }} onPress={()=> {
-                            createFileConfig(config);
-                        }}>
-                            <Text>Salvar</Text>
-                        </Button>
                     </View>
                 </Content>
             </Container>
@@ -178,7 +206,7 @@ export default class Config extends Component {
     }
 }
 
-const styles = StyleSheet.create({
+const styles2 = StyleSheet.create({
     container: {
         backgroundColor: '#F5FCFF',
     },

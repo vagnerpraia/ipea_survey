3d2041ca8addf2764f7f6f359f36149a464930e7
vagnerpraia <vagnerpraia@gmail.com>
2017-05-07 12:21:27 -0300

    Ajuste configuração

--- app/component/config/Config.js
+++ app/component/config/Config.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, ToastAndroid, View } from 'react-native';
 import { Button, Container, Content, List, Header, Icon, ListItem, Picker, Text, Title } from 'native-base';
 import TextField from 'react-native-md-textinput';
 import RadioForm from 'react-native-simple-radio-button';
@@ -17,17 +17,30 @@ var dir_base = dirs.DownloadDir.substring(0, dirs.DownloadDir.lastIndexOf('/') +
 var dir_file = dir_base + 'Ipea/IpeaSurvey/';
 
 export default class Config extends Component {
-    state = {
-      nome_aplicador: '',
-      nome_entrevistado: '',
-      selected_uf: 'ac',
-      selected_municipio: 0,
-      localidade: '',
-      localizacao: '',
-      loc_diferenciada: '',
-      barragem: '',
+    constructor(props) {
+        super(props);
 
-      mode: Picker.MODE_DIALOG,
+        let file_path = dir_file + 'config.json';
+        fs.exists(file_path).then((exist) => {
+            if(exist){
+                fs.readFile(file_path, 'utf8').then((data) => {
+                    console.log(data);
+                });
+            }
+        });
+    }
+
+    state = {
+        nome_aplicador: '',
+        nome_entrevistado: '',
+        selected_uf: 'ac',
+        selected_municipio: 0,
+        localidade: '',
+        localizacao: '',
+        loc_diferenciada: '',
+        barragem: '',
+
+        mode: Picker.MODE_DIALOG,
     }
 
     render() {
@@ -36,29 +49,29 @@ export default class Config extends Component {
         var make = UF_MUNICIPIO[this.state.selected_uf];
 
         var config = {
-          "nome_aplicador": nome_aplicador,
-          "nome_entrevistado": nome_entrevistado,
-          "uf": make.name,
-          "municipio": make.municipios[this.state.selected_municipio],
-          "localidade": localidade,
-          "localizacao": localizacao,
-          "loc_diferenciada": loc_diferenciada,
-          "barragem": barragem
+            "nome_aplicador": nome_aplicador,
+            "nome_entrevistado": nome_entrevistado,
+            "uf": make.name,
+            "municipio": make.municipios[this.state.selected_municipio],
+            "localidade": localidade,
+            "localizacao": localizacao,
+            "loc_diferenciada": loc_diferenciada,
+            "barragem": barragem
         };
 
         var createFileConfig = (content) => {
-          var file_path = dir_file + 'config.json';
-          var data = JSON.stringify(content);
-          fs.exists(file_path).then((exist) => {
-              if(exist){
-                fs.writeFile(file_path, data, 'utf8').then(() => {
-                console.log('Arquivo atualizado');
-                });
-              }else{
-                  fs.createFile(file_path, data, 'utf8');
-                  console.log('Arquivo criado');
-              }
-          });
+            var file_path = dir_file + 'config.json';
+            var data = JSON.stringify(content);
+            fs.exists(file_path).then((exist) => {
+                if(exist){
+                    fs.writeFile(file_path, data, 'utf8').then(() => {
+                        ToastAndroid.showWithGravity('Configração salva', ToastAndroid.SHORT, ToastAndroid.CENTER);
+                    });
+                }else{
+                    fs.createFile(file_path, data, 'utf8');
+                    ToastAndroid.showWithGravity('Configração salva', ToastAndroid.SHORT, ToastAndroid.CENTER);
+                }
+            });
         };
 
         return (
@@ -75,89 +88,90 @@ export default class Config extends Component {
                     </Button>
                 </Header>
                 <Content>
-                <View style={styles.view}>
-                  <TextField
-                      label={'Nome do aplicador'}
-                      value={nome_aplicador}
-                      labelColor={'#000000'}
-                      onChangeText={ (nome_aplicador) => this.setState({ nome_aplicador }) }
-                      highlightColor={'#00BCD4'}
-                      dense={true}
-                  />
-                  <TextField
-                      label={'Nome do entrevistado'}
-                      value={nome_entrevistado}
-                      labelColor={'#000000'}
-                      onChangeText={ (nome_entrevistado) => this.setState({ nome_entrevistado }) }
-                      highlightColor={'#00BCD4'}
-                      dense={true}
-                  />
-                  <Text style={styles.texttitle} >UF</Text>
-                  <Picker
-                    selectedValue={this.state.selected_uf}
-                    onValueChange={(selected_uf) => this.setState({selected_uf, selected_municipio: 0})}>
-                    {Object.keys(UF_MUNICIPIO).map((selected_uf) => (
-                      <Item
-                        key={selected_uf}
-                        value={selected_uf}
-                        label={UF_MUNICIPIO[selected_uf].name}
-                      />
-                    ))}
-                  </Picker>
-                  <Text style={styles.texttitle} >Município</Text>
-                  <Picker
-                    selectedValue={this.state.selected_municipio}
-                    key={this.state.selected_uf}
-                    onValueChange={(selected_municipio) => this.setState({selected_municipio})}>
-                    {UF_MUNICIPIO[this.state.selected_uf].municipios.map((modelName, selected_municipio) => (
-                      <Item
-                        key={this.state.selected_uf + '_' + selected_municipio}
-                        value={selected_municipio}
-                        label={modelName}
-                      />
-                    ))}
-                  </Picker>
-                  <TextField
-                        label={'Localidade '}
-                        value={localidade}
-                        labelColor={'#000000'}
-                        onChangeText={ (localidade) => this.setState({ localidade }) }
-                        highlightColor={'#00BCD4'}
-                        dense={true}
-                  />
-                  <Text style={styles.texttitle} >Localização/Zona</Text>
-                  <RadioForm
-                    radio_props={radio_localizacao}
-                    initial={null}
-                    buttonColor={'#000000'}
-                    buttonSize={8}
-                    labelStyle={styles.radioLabel}
-                    style={styles.radioForm}
-                    onPress={(localizacao) => {this.setState({localizacao})}}
-                  />
-                  <Text style={styles.texttitle} >Localização diferenciada</Text>
-                  <RadioForm
-                    radio_props={radio_loc_diferenciada}
-                    initial={null}
-                    buttonColor={'#000000'}
-                    buttonSize={8}
-                    labelStyle={styles.radioLabel}
-                    style={styles.radioForm}
-                    onPress={(loc_diferenciada) => {this.setState({loc_diferenciada})}}
-                  />
-                  <TextField
-                        label={'Barragem'}
-                        value={barragem}
-                        labelColor={'#000000'}
-                        onChangeText={ (barragem) => this.setState({ barragem }) }
-                        highlightColor={'#00BCD4'}
-                        dense={true}
-                  />
-                  <Button
-                    style={{ marginTop: 10 }}
-                    onPress={()=> { createFileConfig(config); }}
-                    > Salvar </Button>
-                </View>
+                    <View style={styles.view}>
+                        <TextField
+                            label={'Nome do aplicador'}
+                            value={nome_aplicador}
+                            labelColor={'#000000'}
+                            onChangeText={ (nome_aplicador) => this.setState({ nome_aplicador }) }
+                            highlightColor={'#00BCD4'}
+                            dense={true}
+                        />
+                        <TextField
+                            label={'Nome do entrevistado'}
+                            value={nome_entrevistado}
+                            labelColor={'#000000'}
+                            onChangeText={ (nome_entrevistado) => this.setState({ nome_entrevistado }) }
+                            highlightColor={'#00BCD4'}
+                            dense={true}
+                        />
+                        <Text style={styles.texttitle} >UF</Text>
+                        <Picker
+                            selectedValue={this.state.selected_uf}
+                            onValueChange={(selected_uf) => this.setState({selected_uf, selected_municipio: 0})}>
+                            {Object.keys(UF_MUNICIPIO).map((selected_uf) => (
+                                <Item
+                                    key={selected_uf}
+                                    value={selected_uf}
+                                    label={UF_MUNICIPIO[selected_uf].name}
+                                />
+                            ))}
+                        </Picker>
+                        <Text style={styles.texttitle} >Município</Text>
+                        <Picker
+                            selectedValue={this.state.selected_municipio}
+                            key={this.state.selected_uf}
+                            onValueChange={(selected_municipio) => this.setState({selected_municipio})}>
+                            {UF_MUNICIPIO[this.state.selected_uf].municipios.map((modelName, selected_municipio) => (
+                                <Item
+                                    key={this.state.selected_uf + '_' + selected_municipio}
+                                    value={selected_municipio}
+                                    label={modelName}
+                                    />
+                            ))}
+                        </Picker>
+                        <TextField
+                            label={'Localidade '}
+                            value={localidade}
+                            labelColor={'#000000'}
+                            onChangeText={ (localidade) => this.setState({ localidade }) }
+                            highlightColor={'#00BCD4'}
+                            dense={true}
+                        />
+                        <Text style={styles.texttitle} >Localização/Zona</Text>
+                        <RadioForm
+                            radio_props={radio_localizacao}
+                            initial={null}
+                            buttonColor={'#000000'}
+                            buttonSize={8}
+                            labelStyle={styles.radioLabel}
+                            style={styles.radioForm}
+                            onPress={(localizacao) => {this.setState({localizacao})}}
+                        />
+                        <Text style={styles.texttitle} >Localização diferenciada</Text>
+                        <RadioForm
+                            radio_props={radio_loc_diferenciada}
+                            initial={null}
+                            buttonColor={'#000000'}
+                            buttonSize={8}
+                            labelStyle={styles.radioLabel}
+                            style={styles.radioForm}
+                            onPress={(loc_diferenciada) => {this.setState({loc_diferenciada})}}
+                        />
+                        <TextField
+                            label={'Barragem'}
+                            value={barragem}
+                            labelColor={'#000000'}
+                            onChangeText={ (barragem) => this.setState({ barragem }) }
+                            highlightColor={'#00BCD4'}
+                            dense={true}
+                        />
+                        <Button style={{ marginTop: 10 }} onPress={()=> {
+                            createFileConfig(config);
+                        }}>
+                            <Text>Salvar</Text>
+                        </Button>
+                    </View>
                 </Content>
             </Container>
         );
@@ -169,15 +183,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
     },
     view: {
-      flex: 1,
-      flexDirection: 'column',
-      padding: 15,
-      marginTop: -15
+        flex: 1,
+        flexDirection: 'column',
+        padding: 15,
+        marginTop: -15
     },
     texttitle: {
-      color: '#000000',
-      marginTop: 10,
-      fontSize: 13,
+        color: '#000000',
+        marginTop: 10,
+        fontSize: 13,
     },
     radioLabel: {
         fontSize: 13,
